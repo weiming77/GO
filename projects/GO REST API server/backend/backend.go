d9f7799f4a5cbc3c2f0c879dd641b01867e71ffd
Wei Ming <weiming77@gmail.com>
2023-05-18 23:31:16 +0800

    using a router

--- projects/GO REST API server/backend/backend.go
+++ projects/GO REST API server/backend/backend.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 
+	//use gorila mux when response differently based on API method call
+	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -32,12 +34,60 @@ type Movie struct {
 	year     int
 }
 
+// default root handle
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
 }
 
+// This is get handle - Data retrieve/view
+func getRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a GET")
+}
+
+// This is a POST handle - New data
+func postRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a POST")
+}
+
+// This is a PUT handle - Update several/partial data
+func putRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a PUT")
+}
+
+// This is a PATCH handle - update several/partial data
+func patchRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a PATCH")
+}
+
+// This is a DELETE handle - update several/partial data
+func deleteRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a DELETE")
+}
+
 func (a *App) Run() {
-	http.HandleFunc("/", helloWorld)
+
+	// APPSERVER is returning "hello world" when request match the end point in regardless of different REST methods
+	//http.HandleFunc("/", helloWorld)
+
+	// Ideally we want differert API methods handled by different GO methods.
+	// In other words we want different request handlers for one model ie products, orders
+	// to accomplish this, we would use a router. a router keep track of what code to execute (controller) based on the API endpoint and method called.
+	// I like to use router for better code layout, as I can group together API handlers base on the resource (model) they affect
+	// use router so http server response according to the APIs method used.
+	r := mux.NewRouter()
+	// curl -i http://localhost:3030
+	r.HandleFunc("/", getRequest).Methods(http.MethodGet)
+	// curl -i -X POST http://localhost:3030
+	r.HandleFunc("/", postRequest).Methods(http.MethodPost)
+	// curl -i -X PUT http://localhost:3030
+	r.HandleFunc("/", putRequest).Methods(http.MethodPut)
+	// curl -i -X PATCH http://localhost:3030
+	r.HandleFunc("/", patchRequest).Methods(http.MethodPatch)
+	// curl -i -X DELETE http://localhost:3030
+	r.HandleFunc("/", deleteRequest).Methods(http.MethodDelete)
+	// This is how one end point react differently to different APIs method
+	http.Handle("/", r)
+
 	fmt.Println("Server started and listening on port", a.Port)
 	log.Fatal(http.ListenAndServe(a.Port, nil))
 }
