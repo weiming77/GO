e80e7de645535329de86cdb2c1fa0ed3ec77135c
Wei Ming <weiming77@gmail.com>
2023-05-18 22:24:57 +0800

    fix typo in the comment

--- projects/GO REST API server/module_referencing/tester/tester.go
+++ projects/GO REST API server/module_referencing/tester/tester.go
@@ -8,7 +8,7 @@ func main() {
 	// Since we are yet to publish the practice pakcage, We will run into the error:
 	// no required module provides package example.com/practice; to add it: go get example.com/practice
 	// when we go run tester.go
-	// Resolution is to give go.mod some helps via command
+	// Resolution is to give go.mod some helps via commands:
 	// go mod edit -replace example.com/practice=../practice
 	// go mod tidy
 	practice.Test()
