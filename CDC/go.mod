2ebe13feca2460e138de0544fb4d62d7cefbe368
Wei Ming <weiming77@gmail.com>
2023-08-01 17:25:59 +0800

    configuration of data import insert scripts

---
+++ CDC/go.mod
@@ -0,0 +1,3 @@
+module github.com/weiming77/GO/CDC
+
+go 1.20
---
+++ CDC/porting/portConf.go
@@ -0,0 +1,40 @@
+package porting
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const CONFIG_FILE = "..\\portConf.json"
+
+type PortFile struct {
+	Table  string `json:"table"`
+	Schema string `json:"schema"`
+	File   string `json:"file"`
+}
+
+type Configuration struct {
+	Tables []PortFile `json:""`
+}
+
+var PortConfiguration Configuration = Configuration{}
+
+func init() {
+	fileConf, err := os.Open(CONFIG_FILE)
+	if err != nil {
+		log.Fatalf("File open\n%v\n", err)
+	}
+	defer fileConf.Close()
+
+	dataConf, err := ioutil.ReadAll(fileConf)
+	if err != nil {
+		log.Fatalf("File read\n%v\n", err)
+	}
+
+	err = json.Unmarshal(dataConf, &PortConfiguration)
+	if err != nil {
+		log.Fatalf("File load\n%v\n", err)
+	}
+}
