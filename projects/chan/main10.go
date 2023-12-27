8f9bb494d136a7c295279abac2d5bf28055ba8f9
Wei Ming <weiming77@gmail.com>
2023-12-28 00:52:39 +0800

    return nxnumbers from generic generator

--- projects/chan/main10.go
+++ projects/chan/main10.go
@@ -21,12 +21,28 @@ func generatorFunc[T any, K any](done <-chan K, fn func() T) <-chan T {
 	return stream
 }
 
+func take[T any, K any](done <-chan K, stream <-chan T, n int) <-chan T {
+	taken := make(chan T)
+	go func() {
+		defer close(taken)
+		for i := 0; i < n; i++ {
+			select {
+			case <-done:
+				return
+			case taken <- <-stream:
+			}
+		}
+	}()
+
+	return taken
+}
+
 func main() {
 	done := make(chan int)
 	defer close(done)
 
 	randNumFetcher := func() int { return rand.Intn(500000000) }
-	for rando := range generatorFunc(done, randNumFetcher) {
+	for rando := range take(done, generatorFunc(done, randNumFetcher), 10) {
 		fmt.Println(rando)
 	}
 }
