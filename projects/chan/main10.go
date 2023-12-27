7cef8eb27fa1553c0afc548d659f8bdd05232ea9
Wei Ming <weiming77@gmail.com>
2023-12-28 01:21:44 +0800

    slow and need fan out

--- projects/chan/main10.go
+++ projects/chan/main10.go
@@ -37,12 +37,41 @@ func take[T any, K any](done <-chan K, stream <-chan T, n int) <-chan T {
 	return taken
 }
 
+func primeFinder(done <-chan int, randIntStream <-chan int) <-chan int {
+	isPrime := func(randomInt int) bool {
+		for i := randomInt - 1; i > 1; i-- {
+			if randomInt%i == 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	primes := make(chan int)
+	go func() {
+		defer close(primes)
+		for {
+			select {
+			case <-done:
+				return
+			case randomInt := <-randIntStream:
+				if isPrime(randomInt) {
+					primes <- randomInt
+				}
+			}
+		}
+	}()
+	return primes
+}
+
 func main() {
 	done := make(chan int)
 	defer close(done)
 
 	randNumFetcher := func() int { return rand.Intn(500000000) }
-	for rando := range take(done, generatorFunc(done, randNumFetcher), 10) {
+	randIntStream := generatorFunc(done, randNumFetcher)
+	primeStream := primeFinder(done, randIntStream)
+	for rando := range take(done, primeStream, 10) {
 		fmt.Println(rando)
 	}
 }
